9c4fef28421b1db4b5c6a00023e7abf72b3ad175
Rahul <40555494+Rahulki@users.noreply.github.com>
2025-01-27 23:11:32 -0500

    moved nav.css to Css folder

--- src/Components/Nav.jsx
+++ src/Components/Nav.jsx
@@ -1,6 +1,6 @@
 import { useState } from "react";
 import { Link } from "react-router-dom";
-import "./Nav.css";
+import "../Css/Nav.css";
 
 const Nav = () =>{
   const [menuOpen, setMenuOpen] = useState(false);
