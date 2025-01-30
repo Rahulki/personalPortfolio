1a773ae2db9f46384ee336f1d9bf1b93426599e4
Rahul <40555494+Rahulki@users.noreply.github.com>
2025-01-29 19:39:23 -0500

    Fix Github Pages rounting

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { HashRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './Pages/Home';
 import About from './Pages/About';
 import Education from './Pages/Education';
 import Nav from './Components/Nav';
-import Footer from './Components/Footer'
+import Footer from './Components/Footer';
 import './Css/App.css';
 
 function App() {
